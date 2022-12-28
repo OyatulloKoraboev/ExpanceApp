2c04fa91e2a917bc64ee0c645f685420e5221615
Oyatullo <korabeov.o@gmail.com>
2022-12-28 16:33:37 +0500

    First Commit

---
+++ ExpanceUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpanceUI
+//
+//  Created by Oyatullo Koraboev on 28/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+
+
---
+++ Pods/Target Support Files/Pods-ExpanceUI/Pods-ExpanceUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExpanceUI : NSObject
+@end
+@implementation PodsDummy_Pods_ExpanceUI
+@end
